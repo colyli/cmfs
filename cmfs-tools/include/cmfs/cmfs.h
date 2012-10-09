e9c756322f6d8b5cb72358f8df89ba501015daf9
Coly Li <i@coly.li>
2012-10-09 18:03:50 +0800

    Add _cmfs_filesys, which is learned from _ocfs2_filesys.

--- cmfs-tools/include/cmfs/cmfs.h
+++ cmfs-tools/include/cmfs/cmfs.h
@@ -2,7 +2,7 @@
  * vim: noexpandtab sw=8 ts=8 sts=0:
  *
  * cmfs.h
- * - originally copied and modified from ocfs2.h
+ * (copied and modified from ocfs2.h)
  *
  * Filesystem object routines for the CMFS userspace library.
  *
@@ -26,10 +26,63 @@
  */
 
 
+#ifndef _FILESYS_H
+#define _FILESYS_H
+
+#include <stdint.h>
+
+
+#include <cmfs-kernel/cmfs_fs.h>
+#include <cmfs/cmfs.h>
+
+
+typedef struct _io_channel io_channel;
+
+
+
+
+
+struct _cmfs_filesys {
+	char *fs_devname;
+	uint32_t fs_flags;
+	io_channel *fs_io;
+	struct cmfs_dinode *fs_super;
+	struct cmfs_dinode *fs_orig_super;
+	unsigned int fs_blocksize;
+	unsigned int fs_clustersize;
+	uint32_t fs_clusters;
+	uint64_t fs_blocks;
+	uint32_t fs_umask;
+	uint64_t fs_root_blkno;
+	uint64_t fs_sysdir_blkno;
+	uint64_t fs_first_cg_blkno;
+	char uuid_str[CMFS_VOL_UUID_LEN * 2 + 1];
+
+	/* Allocators */
+	cmfs_cached_inode *fs_cluster_alloc;
+	cmfs_cached_inode **fs_inode_allocs;
+	cmfs_cached_inode *fs_system_inode_alloc;
+//	cmfs_cached_inode **fs_eb_allocs;
+//	cmfs_cached_inode *fs_system_eb_alloc;
+
+	/* Reserved for the use of the calling application. */
+	void *fs_private;
+};
+
+struct _cmfs_cached_inode {
+	struct _cmfs_filesys *ci_fs;
+	uint64_t ci_blkno;
+	struct cmfs_dinode *ci_inode;
+	cmfs_bitmap *ci_chains;
+}
+
+
+
+
 static inline void cmfs_calc_cluster_groups(
 					uint64_t clusters,
 					uint64_t blocksize,
-					struct ocfs2_cluster_group_sizes *cgs)
+					struct cmfs_cluster_group_sizes *cgs)
 {
 	uint32_t max_bits = 8 * cmfs_group_bitmap_size(blocksize, 0, 0);
 
@@ -43,3 +96,5 @@ static inline void cmfs_calc_cluster_groups(
 	if (cgs->cgs_tail_group_bits == 0)
 		cgs->cgs_tail_group_bits = cgs->cgs_cpg;
 }
+
+#endif
