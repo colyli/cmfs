8689d25534271aaedee986bc000da41e7b25fafa
Coly Li <i@coly.li>
2012-10-09 18:06:32 +0800

    libcmfs: include <cmfs/cmfs.h> in libcmfs/dirblocks.c

--- cmfs-tools/libcmfs/dirblock.c
+++ cmfs-tools/libcmfs/dirblock.c
@@ -1,9 +1,9 @@
-
+#include <cmfs/cmfs.h>
 
 
 
 struct cmfs_dir_block_trailer *
-cmfs_dir_trailer_from_block(ocfs2_filesys *fs, void  *data)
+cmfs_dir_trailer_from_block(cmfs_filesys *fs, void  *data)
 {
 	char *p = data;
 	p += cmfs_dir_trailer_blk_off(fs);
