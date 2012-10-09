817a22de4248207ff4526575cbcc4edb5710e7a6
Coly Li <i@coly.li>
2012-10-09 18:04:36 +0800

    Add a very early version of cmfs_fs.h which will be in kernel code.

---
+++ cmfs-tools/include/cmfs-kernel/cmfs_fs.h
@@ -0,0 +1,36 @@
+/* -*- mode: c; c-basic-offset: 8; -*-
+ * vim: noexpandtab sw=8 ts=8 sts=0:
+ *
+ * cmfs_fs.h
+ * (copied and modified from ocfs2_fs.h from ocfs2-tools)
+ *
+ * On-disk structures for CMFS.
+ *
+ * Copyright (C) 2002, 2004 Oracle.All rights reserved.
+ * CMFS modification, Copyright (C) 2012, Coly Li <i@coly.li>
+ *
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public
+ * License, version 2,as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public
+ * License along with this program; if not, write to the
+ * Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+ * Boston, MA 021110-1307, USA.
+ */
+
+#ifndef _CMFS_FS_H
+#define _CMFS_FS_H
+
+
+#define CMFS_VOL_UUID_LEN		16
+//#define CMFS_MAX_VOL_LABEL_LEN	64
+
+
+#endif
