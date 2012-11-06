cbb1b79e41c586e7076f72a3293d5bd56642537d
Coly Li <i@coly.li>
2012-11-07 00:26:51 +0800

    cmfs-kernel/cmfs_fs.h: minor fixes and new item added
    - Fix offset comment in data structure definition.
    - Add macros,
    	CMFS_MAJOR/MINOR_REV_LEVEL
    	CMFS_MAX_FILENAME_LEN
    	CMFS_VALID_FL
    	CMFS_UNUSED1_FL
    	CMFS_ORPHANED_FL
    	CMFS_UNUSED2_FL
    	CMFS_SYSTEM_FL
    	CMFS_SUPER_BLOCK_FL
    	CMFS_LOCAL_ALLOC_FL
    	CMFS_BITMAP_FL
    	CMFS_JOURNAL_FL
    	CMFS_CHAIN_FL
    	CMFS_DEALLOC_FL
    	CMFS_INLINE_DATA_FL
    	CMFS_HAS_XATTR_FL
    	CMFS_INLINE_XATTR_FL
    	CMFS_INDEXED_DIR_FL
    	CMFS_HAS_REFCOUNT_FL

--- cmfs-tools/include/cmfs-kernel/cmfs_fs.h
+++ cmfs-tools/include/cmfs-kernel/cmfs_fs.h
@@ -30,19 +30,75 @@
 
 #include <linux/types.h>
 
+/* Version */
+#define CMFS_MAJOR_REV_LEVEL		0
+#define CMFS_MINOR_REV_LEVEL		1
+
+/*
+ * A CMFS volume starts this way:
+ * Sector 0: Valid cmfs_vol_disk_hdr
+ * Sector 1: Valid cmfs_vol_label
+ * Block CMFS_SUPER_BLOCK_BLKNO: CMFS usperblock.
+ *
+ * All other structures are found from the superblock information.
+ *
+ * CMFS_SUPER_BLOCK_BLKNO is in blocks other than sectors, it is
+ * 4096 bytes into disk/partition.
+ */
+#define CMFS_SUPER_BLOCK_BLKNO		2
+
 #define CMFS_MAX_VOL_SIGNATURE_LEN	128
 #define CMFS_MAX_MOUNT_POINT_LEN	128
 #define CMFS_MAX_VOL_LABEL_LEN		 64
 #define CMFS_MAX_VOL_ID_LENGTH		 16
 #define CMFS_VOL_UUID_LEN		 16
+#define CMFS_MAX_FILENAME_LEN		255
 
-
+/*
+ * Min block size = Max block size = 4KB
+ * Min cluster size: 4KB
+ * Max cluster size: 32MB
+ */
 #define CMFS_MIN_CLUSTERSIZE	4096
 #define CMFS_MAX_CLUSTERSIZE	(32*(1<<20))
 #define CMFS_MAX_BLOCKSIZE	CMFS_MIN_CLUSTERSIZE
 #define CMFS_MIN_BLOCKSIZE	CMFS_MAX_BLOCKSIZE
 
 #define CMFS_SUPER_BLOCK_SIGNATURE	"CMFSV1"
+
+/*
+ * Flags on cmfs_dinode.i_flags
+ */
+#define CMFS_VALID_FL		(0x00000001)	/* Inode is valid */
+#define CMFS_UNUSED1_FL		(0x00000002)
+#define CMFS_ORPHANED_FL	(0x00000004)	/* On the orphan list */
+#define CMFS_UNUSED2_FL		(0x00000008)
+/* System inode flags */
+#define CMFS_SYSTEM_FL		(0x00000010)	/* System inode */
+#define CMFS_SUPER_BLOCK_FL	(0x00000020)	/* Super block */
+#define CMFS_LOCAL_ALLOC_FL	(0x00000040)	/* Slot local alloc bitmap */
+#define CMFS_BITMAP_FL		(0x00000080)	/* Allocation bitmap */
+#define CMFS_JOURNAL_FL		(0x00000100)	/* Journal */
+#define CMFS_CHAIN_FL		(0x00000200)	/* Chain allocator */
+#define CMFS_DEALLOC_FL		(0x00000400)	/* Truncate log */
+
+/*
+ * Flags on cmfs_dinode.i_dyn_features
+ *
+ * These can change much more often than i_flags. When adding
+ * flags, keep in mind that i_dyn_features in only 16 bits
+ * wide.
+ *
+ * XXX: Currently most of them will not be supported
+ */
+#define CMFS_INLINE_DATA_FL	(0x0001)	/* Data stored in inode block */
+#define CMFS_HAS_XATTR_FL	(0x0002)
+#define CMFS_INLINE_XATTR_FL	(0x0004)
+#define CMFS_INDEXED_DIR_FL	(0x0008)
+#define CMFS_HAS_REFCOUNT_FL	(0x0010)
+
+
+
 /*
  * CMFS volume header, lives at sector 0.
  */
@@ -90,8 +146,10 @@ struct cmfs_extent_rec {
 			__le32 e_leaf_blocks;	/* blocks covered by this extent
 		       				   for 4KB block, the max length
 						   of single extent is 16TB */
-			uint8_t e_reserved1[3];
 			uint8_t e_flags;	/* extent flags */
+			uint8_t e_reserved1;
+			__le16	e_reserved2;
+			__le64	e_reserved3;
 		};
 	};
 /*20*/
@@ -206,14 +264,14 @@ struct cmfs_super_block {
 /*30*/	__le64 s_system_dir_blkno;
 	__le32 s_blocksize_bits;
 	__le32 s_clustersize_bits;
-/*40*/	__le16 s_tunefs_flag;
-	uint8_t s_label[CMFS_MAX_VOL_LABEL_LEN];
-	uint8_t s_uuid[CMFS_VOL_UUID_LEN];
+/*40*/	uint8_t s_label[CMFS_MAX_VOL_LABEL_LEN];
+/*80*/	uint8_t s_uuid[CMFS_VOL_UUID_LEN];
+/*92*/	__le16 s_tunefs_flag;
 	__le32 s_uuid_hash;
 	__le64 s_first_cluster_group;
-/*50*/	__le16 s_xattr_inline_size;
+/*A0*/	__le16 s_xattr_inline_size;
 	__le16 s_reserved1[7];
-/*60*/
+/*B0*/
 //	__le16 s_max_slots; /* XXX: use this for concurrently inode allocation */
 /* XXXX: should pad all rest space of dinode which contains the super block to zero */
 };
@@ -249,10 +307,10 @@ struct cmfs_dinode {
 	__le64 i_refcount_loc;
 /*80*/	__le64 i_suballoc_loc;
 /*88*/ struct cmfs_block_check i_check;
-/*A0*/	__le16 i_dyn_features;
+/*90*/	__le16 i_dyn_features;
 	__le16 i_xattr_inline_size;
 	__le32 i_reserved1[1];
-/*A8*/	union {
+/*98*/	union {
 		__le64 i_pad1;
 		struct {
 			__le64 i_rdev;
@@ -266,7 +324,7 @@ struct cmfs_dinode {
 			__le32 ij_recovery_generation;
 		} journal1;
 	} id1;
-/*B0*/ union {
+/*A0*/ union {
 		struct cmfs_super_block		i_super;
 		struct cmfs_local_alloc		i_lab;
 		struct cmfs_chain_list		i_chain;
@@ -276,6 +334,95 @@ struct cmfs_dinode {
 		uint8_t				i_symlink[0];
 	} id2;
 /* Actual on-disk size is one block (4KB) */
+
 };
+
+/*
+ * On disk directory entry structure for CMFS
+ *
+ * Packed as this structure is variable size and accessed
+ * unsligned on 32/64-bit platform
+ */
+struct cmfs_dir_entry {
+/*00*/	__le64 inode;				/* Inode number */
+	__le16 rec_len;				/* Directory entry length */
+	uint8_t name_len;			/* Name length */
+	uint8_t file_type;
+/*0c*/	char name[CMFS_MAX_FILENAME_LEN];	/* File name */
+/* Actual on-disk length specified by rec_len */
+} __attribute__((packed));
+
+/*
+ * Per-block record for the unindexed directry btree.
+ * This is carefully crafted so that the rec_len and name_len records
+ * of a cmfs_dir_entry are mirrored. That way, the directory manipulation
+ * code needs a minial amount of update.
+ *
+ * NOTE: Keep this structure aligned to a multiple of 4 bytes.
+ */
+struct cmfs_dir_block_trailer {
+/*00*/	__le64	db_compat_inode;	/* Always zero. Was inode */
+	__le16	db_compat_rec_len;	/* Backwards compatible with
+					   cmfs_dir_entry */
+	uint8_t	db_compat_name_len;	/* Always zero. Was name_len */
+	uint8_t	db_reserved0;
+	__le16	db_reserved1;
+	__le16	db_free_rec_len;	/* Size of largest empty hole
+					   in this block. */
+/*10*/	uint8_t	db_signature[8];	/* Signature for verification */
+	__le64	db_reserved2;
+/*20*/	__le64	db_free_next;		/* Next block in list */
+	__le64	db_blkno;		/* Offset o disk, in blocks */
+/*30*/	__le64	db_parent_dinode;	/* dinode which owns me, in
+					   blocks */
+	__le64	db_reserved3;
+/*40*/	struct cmfs_block_check db_check; /* Error checking */
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 #endif
