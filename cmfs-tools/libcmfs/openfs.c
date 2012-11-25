43d039e7021a3cc082f94f200ed434398551f965
Coly Li <i@coly.li>
2012-11-26 01:22:08 +0800

    libcmfs: add cmfs_read_super() to openfs.c
    This patch adds cmfs_read_super() and other routines depended by
    cmfs_read_super() in libcmfs/openfs.c. All new added routines includes,
    - cmfs_read_super()
    - cmfs_read_blocks()
    - __cmfs_read_block()

--- cmfs-tools/libcmfs/openfs.c
+++ cmfs-tools/libcmfs/openfs.c
@@ -31,8 +31,104 @@
 #include <string.h>
 #include <et/com_err.h>
 
+#include <cmfs-kernel/cmfs_fs.h>
 #include <cmfs/cmfs.h>
+#include "cmfs_err.h"
 
+
+
+
+
+static errcode_t __cmfs_read_blocks(cmfs_filesys *fs,
+				    uint64_t blkno,
+				    int count,
+				    char *data,
+				    int nocache)
+{
+	errcode_t err;
+
+	if (nocache)
+		err = io_read_block_nocache(fs->fs_io, blkno, count, data);
+	else
+		err = io_read_block(fs->fs_io, blkno, count, data);
+
+	return err;
+}
+
+errcode_t cmfs_read_blocks(cmfs_filesys *fs,
+			   uint64_t blkno,
+			   int count,
+			   char *data)
+{
+	return __cmfs_read_blocks(fs, blkno, count, data, 0);
+}
+
+errcode_t cmfs_read_super(cmfs_filesys *fs, uint64_t superblock, char *sb)
+{
+	errcode_t ret;
+	char *blk, *swapblk;
+	struct cmfs_dinode *di, *orig_super;
+	int orig_blocksize;
+	int blocksize = io_get_blksize(fs->fs_io);
+
+	ret = cmfs_malloc_block(fs->fs_io, &blk);
+	if (ret)
+		return ret;
+
+	ret = cmfs_read_blocks(fs, superblock, 1, blk);
+	if (ret)
+		goto out_blk;
+
+	di = (struct cmfs_dinode *)blk;
+
+	ret = CMFS_ET_BAD_MAGIC;
+	if (memcmp(di->i_signature,
+		   CMFS_SUPER_BLOCK_SIGNATURE,
+		   strlen(CMFS_SUPER_BLOCK_SIGNATURE)))
+		goto out_blk;
+
+	/*
+	 * when calling cmfs_validate_meta_ecc() to validate super block,
+	 * flags in fs->fs_super hash to be check whether meta ecc is
+	 * enabled in the superblock (blk read by cmfs_read_blocks()).
+	 * Therefore, before calling cmfs_validate_meta_ecc() to validate
+	 * blk, a swapblk copy is needed, which is used to replace
+	 * fs->fs_super and fs->fs_blocksize.
+	 */
+	ret = cmfs_malloc_block(fs->fs_io, &swapblk);
+	if (ret)
+		goto out_blk;
+
+	memcpy(swapblk, blk, blocksize);
+	orig_super = fs->fs_super;
+	orig_blocksize = fs->fs_blocksize;
+	fs->fs_super = (struct cmfs_dinode *)swapblk;
+	fs->fs_blocksize = blocksize;
+	cmfs_swap_inode_to_cpu(fs, fs->fs_super);
+
+	ret = cmfs_validate_meta_ecc(fs, blk, &di->i_check);
+
+	fs->fs_super = orig_super;
+	fs->fs_blocksize = orig_blocksize;
+	cmfs_free(&swapblk);
+
+	if (ret)
+		goto out_blk;
+
+	/* meta ecc check OK, now update to fs->fs_super */
+	cmfs_swap_inode_to_cpu(fs, di);
+	if (!sb)
+		fs->fs_super = di;
+	else {
+		memcpy(sb, blk, fs->fs_blocksize);
+		cmfs_free(&blk);
+	}
+
+	return 0;
+out_blk:
+	cmfs_free(&blk);
+	return ret;
+}
 
 errcode_t cmfs_open(const char *name,
 		    int flags,
@@ -71,24 +167,12 @@ errcode_t cmfs_open(const char *name,
 		goto out;
 	strcpy(fs->fs_devname, name);
 
-	/*
-	 * If CMFS_FLAG_IMAGE_FILE is specified, it needs to be
-	 * handled differently
-	 */
-	if (flags & CMFS_FLAG_IMAGE_FILE) {
-		ret = cmfs_image_load_bitmap(fs);
-		if (ret)
-			goto out;
-		if (!superblock)
-			superblock = fs->ost->ost_superblocks[0];
-	} else {
-		if (io_is_device_readonly(fs->fs_io))
+	/* don't support image file yet */
+	if (io_is_device_readonly(fs->fs_io))
 			fs->fs_flags |= CMFS_FLAG_HARD_RO;
-	}
-
 
 	if (!superblock)
-		superblock = CMFS_SUPER_BLOCK_NO;
+		superblock = CMFS_SUPER_BLOCK_BLKNO;
 	io_set_blksize(fs->fs_io, block_size);
 
 	ret = cmfs_read_super(fs, (uint64_t)superblock, NULL);
@@ -104,17 +188,6 @@ errcode_t cmfs_open(const char *name,
 			(char *)fs->fs_super,
 			fs->fs_blocksize);
 	}
-
-	ret = CMFS_ET_UNSUPP_FEATURE;
-	if (CMFS_RAW_SB(fs->fs_super)->s_feature_incompat &
-	    ~CMFS_LIB_FEATURE_INCOMPAT_SUPP)
-		goto out;
-
-	ret = CMFS_ET_RO_UNSUPP_FEATURE;
-	if((flags & CMFS_FLAG_RW) &&
-	   (CMFS_RAW_SB(fs->fs_super)->s_feature_ro_compat &
-	    ~CMFS_LIB_FEATURE_RO_COMPAT_SUPP))
-		goto out;
 
 	ret = CMFS_ET_CORRUPT_SUPERBLOCK;
 	if (!CMFS_RAW_SB(fs->fs_super)->s_blocksize_bits)
@@ -132,24 +205,24 @@ errcode_t cmfs_open(const char *name,
 		goto out;
 
 	ret = CMFS_ET_UNEXPECTED_BLOCK_SIZE;
-	if ((1 << CMFS_RAW_SB(fs->super)->s_blocksize_bits) !=
+	if ((1 << CMFS_RAW_SB(fs->fs_super)->s_blocksize_bits) !=
 	    CMFS_MAX_BLOCKSIZE)
 		goto out;
 
 	fs->fs_clustersize =
-		1 << CMFS_RAW_SB(fs->super)->s_clustersize_bits;
+		1 << CMFS_RAW_SB(fs->fs_super)->s_clustersize_bits;
 
 	/* XXX: read system dir */
 	fs->fs_root_blkno =
-		CMFS_RAW_SB(fs->super)->s_root_blkno;
+		CMFS_RAW_SB(fs->fs_super)->s_root_blkno;
 	fs->fs_sysdir_blkno =
-		CMFS_RAW_SB(fs->super)->s_system_dir_blkno;
+		CMFS_RAW_SB(fs->fs_super)->s_system_dir_blkno;
 	fs->fs_clusters = fs->fs_super->i_clusters;
 	fs->fs_blocks = cmfs_clusters_to_blocks(fs, fs->fs_clusters);
 	fs->fs_first_cg_blkno =
 		CMFS_RAW_SB(fs->fs_super)->s_first_cluster_group;
 
-	raw_uuid = CMFS_RAW_SB(fs->super)->s_uuid;
+	raw_uuid = CMFS_RAW_SB(fs->fs_super)->s_uuid;
 	for (i = 0, ptr = fs->uuid_str; i < CMFS_VOL_UUID_LEN; i++) {
 		/* print with null */
 		len = snprintf(ptr, 3, "%02X", raw_uuid[i]);
@@ -167,7 +240,6 @@ out:
 	cmfs_freefs(fs);
 	return ret;
 }
-
 
 
 
