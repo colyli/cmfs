ab4ffff29c6da236c031ebbcc8939b0822cec893
Coly Li <i@coly.li>
2012-12-06 15:10:32 +0800

    cmfs-tools: initial commit of include/cmfs-kernel/cmfs_ioctl.h

---
+++ cmfs-tools/include/cmfs-kernel/cmfs_ioctl.h
@@ -0,0 +1,28 @@
+/* -*- mode: c; c-basic-offset: 8; -*-
+ * vim: noexpandtab sw=8 ts=8 sts=0:
+ *
+ * cmfs_ioctl.h
+ * (Copied and modified from ocfs2-tools/include/ocfs2-kernel/ocfs2_ioctl.h)
+ *
+ * Defines CMFS ioctls.
+ *
+ * Copyright (C) 2010 Oracle.  All rights reserved.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public
+ * License, version 2, as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ */
+
+#ifndef OCFS2_IOCTL_H
+#define OCFS2_IOCTL_H
+
+
+#define CMFS_TEXT_UUID_LEN	(CMFS_VOL_UUID_LEN * 2)
+#define CMFS_INFO_MAX_HIST	(32)
+
+#endif
