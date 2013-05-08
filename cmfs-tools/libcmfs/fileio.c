a4582c6ff2682e1456bcd9f95f8f76d44d5911de
Coly Li <i@coly.li>
2013-05-09 00:42:15 +0800

    cmfs-tools: implement cmfs_file_read() in libcmfs
    This commit implements cmfs_file_read() in libcmfs/fileio.c, which read
    a file region (may be whole file) from disk into a memory buffer.

--- cmfs-tools/libcmfs/fileio.c
+++ cmfs-tools/libcmfs/fileio.c
@@ -33,6 +33,7 @@
 #include <inttypes.h>
 
 #include <cmfs/cmfs.h>
+#include "cmfs_err.h"
 
 struct read_whole_context {
 	char		*buf;
@@ -48,5 +49,82 @@ errcode_t cmfs_file_read(cmfs_cached_inode *ci,
 			 uint64_t offset,
 			 uint32_t *got)
 {
-	return -1;
+	cmfs_filesys *fs = ci->ci_fs;
+	errcode_t ret = 0;
+	char *ptr = buf;
+	uint32_t wanted_blocks;
+	uint64_t contig_blocks;
+	uint64_t v_blkno;
+	uint64_t p_blkno;
+	uint32_t tmp;
+	uint64_t num_blocks;
+	uint16_t extent_flags;
+
+	/*
+	 * o_direct requires aligned io, here we use
+	 * blocksize bytes alignment
+	 */
+	tmp = fs->fs_blocksize -1;
+	if ((count & tmp) || (offset & (uint64_t)tmp) ||
+	    ((unsigned long)ptr & tmp))
+		return CMFS_ET_INVALID_ARGUMENT;
+
+	wanted_blocks = count >> CMFS_RAW_SB(fs->fs_super)->s_blocksize_bits;
+	v_blkno = offset >> CMFS_RAW_SB(fs->fs_super)->s_blocksize_bits;
+	*got = 0;
+
+	num_blocks = (ci->ci_inode->i_size + fs->fs_blocksize - 1) >>
+			CMFS_RAW_SB(fs->fs_super)->s_blocksize_bits;
+
+	if (v_blkno >= num_blocks)
+		return 0;
+
+	if (v_blkno + wanted_blocks > num_blocks)
+		wanted_blocks = (uint32_t)(num_blocks - v_blkno);
+
+	while(wanted_blocks) {
+		ret = cmfs_extent_map_get_blocks(ci,
+						 v_blkno,
+						 1,
+						 &p_blkno,
+						 &contig_blocks,
+						 &extent_flags);
+		if (ret)
+			return ret;
+
+		if (contig_blocks > wanted_blocks)
+			contig_blocks = wanted_blocks;
+
+		if (!p_blkno || extent_flags & CMFS_EXT_UNWRITTEN) {
+			/*
+			 * A hole or unwritten extent met,
+			 * so just empty the content
+			 */
+			memset(ptr, 0, contig_blocks * fs->fs_blocksize);
+		} else {
+			ret = cmfs_read_blocks(fs,
+					       p_blkno,
+					       contig_blocks,
+					       ptr);
+			if (ret)
+				return ret;
+		}
+
+		*got += (contig_blocks <<
+			 CMFS_RAW_SB(fs->fs_super)->s_blocksize_bits);
+		wanted_blocks -= contig_blocks;
+
+		if (wanted_blocks) {
+			ptr += (contig_blocks <<
+				(CMFS_RAW_SB(fs->fs_super)->s_blocksize_bits));
+			v_blkno += (uint64_t)contig_blocks;
+		} else {
+			if (*got + offset > ci->ci_inode->i_size)
+				*got = (uint32_t)
+					(ci->ci_inode->i_size - offset);
+			/* quit while() */
+		}
+	};
+
+	return ret;
 }
