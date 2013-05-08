1bfacfddb8115a41acaa99ad026f6da15d76a345
Coly Li <i@coly.li>
2013-05-09 00:43:30 +0800

    cmfs-tools: implement more routines into libcmfs/cached_inode.c
    This commit implement 2 stub routines in libcmfs/cached_inode.c:
    - cmfs_read_cached_inode()
    - cmfs_free_cached_inode()
    All code is copied and modified from ocfs2-tools/libocfs2/cached_inode.c

--- cmfs-tools/libcmfs/cached_inode.c
+++ cmfs-tools/libcmfs/cached_inode.c
@@ -28,16 +28,59 @@
 #include <string.h>
 
 #include <cmfs/cmfs.h>
+#include "cmfs_err.h"
 
 errcode_t cmfs_read_cached_inode(cmfs_filesys *fs,
 				 uint64_t blkno,
 				 cmfs_cached_inode **ret_ci)
 {
-	return -1;
+	errcode_t ret;
+	char *blk;
+	cmfs_cached_inode *cinode;
+
+	if ((blkno < CMFS_SUPER_BLOCK_BLKNO) ||
+	    (blkno > fs->fs_blocks))
+		return CMFS_ET_BAD_BLKNO;
+
+	ret = cmfs_malloc0(sizeof(cmfs_cached_inode), &cinode);
+	if (ret)
+		return ret;
+
+	cinode->ci_fs = fs;
+	cinode->ci_blkno = blkno;
+
+	ret = cmfs_malloc_block(fs->fs_io, &blk);
+	if (ret)
+		goto cleanup;
+
+	cinode->ci_inode = (struct cmfs_dinode *)blk;
+	ret = cmfs_read_inode(fs, blkno, blk);
+	if (ret)
+		goto cleanup;
+
+	*ret_ci = cinode;
+
+	return 0;
+
+cleanup:
+	cmfs_free_cached_inode(fs, cinode);
+	return ret;
+
 }
 
 errcode_t cmfs_free_cached_inode(cmfs_filesys *fs,
 				 cmfs_cached_inode *cinode)
 {
-	return -1;
+	if (!cinode)
+		return CMFS_ET_INVALID_ARGUMENT;
+
+	if (cinode->ci_chains)
+		cmfs_bitmap_free(cinode->ci_chains);
+
+	if (cinode->ci_inode)
+		cmfs_free(&cinode->ci_inode);
+
+	cmfs_free(&cinode);
+
+	return 0;
 }
